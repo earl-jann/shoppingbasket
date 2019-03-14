eb301f771df062b447417967d1143cbaee6f4628
Earl <earljanntormes@gmail.com>
2019-03-14 18:15:46 +0800

    README

--- src/main/java/com/tormes/shoppingbasket/repository/ProductRepository.java
+++ src/main/java/com/tormes/shoppingbasket/repository/ProductRepository.java
@@ -1,10 +1,9 @@
 package com.tormes.shoppingbasket.repository;
-package com.tormes.shoppingbasket.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.tormes.shoppingbasket.models.Customer;
+import com.tormes.shoppingbasket.models.Product;
 
-public interface ProductRepository extends CrudRepository<Customer, Long>{
+public interface ProductRepository extends CrudRepository<Product, Long>{
 
 }
---
+++ src/test/java/com/tormes/shoppingbasket/GofluentShoppingbasketApplicationTests.java
@@ -0,0 +1,5 @@
+package com.tormes.shoppingbasket;
+
+public class GofluentShoppingbasketApplicationTests {
+
+}
